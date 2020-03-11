cbd792084453f95b7316caab829e8c04cca3bd6f
s-prianto <prianto2796@gmail.com>
2020-03-11 07:15:04 +0700

    Tugas 6 JS

---
+++ src/Lib/DaftarMakanan.js
@@ -0,0 +1,29 @@
+const DaftarMakanan = [
+	{
+		NamaMakanan:"Nasi Padang",
+		Harga:"Rp.20.000",
+		img:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg"
+	},
+	{
+		NamaMakanan:"Sate",
+		Harga:"Rp.10.000",
+		img:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+	},
+	{
+		NamaMakanan:"Soto",
+		Harga:"Rp.10.000",
+		img:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png"
+	},
+	{
+		NamaMakanan:"Uduk",
+		Harga:"Rp.10.000",
+		img:"https://www.dbs.com/iwov-resources/images/newsroom/indonesia/Blog/masakan%20nusantara/nasi%20kentut.png"
+	},
+	{
+		NamaMakanan:"Nasi Kuning",
+		Harga:"Rp.10.000",
+		img:"https://pesona.travel/media/nasi-kuning-sarapan-khas-nusantara-bermakna-mendalam_144651_1140.jpg"
+	},
+];
+
+export default DaftarMakanan;
